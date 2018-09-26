c58f122bfa1b582a6e7082a744469b3be6ace7e8
cuitao <cuitaocrazy@163.com>
2018-09-26 16:05:14 +0800

    print hostname

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const express = require('express')
-const os = require('os')
 
 const app = express()
 
 app.get('/', (req, res) => {
-  const ifs = os.networkInterfaces()
-  const msg = Object.keys(ifs)
-    .map(ifName => ifs[ifName].map(o => `${ifName}: ${o.address}`))
-    .reduce((s, e) => {
-      s.push(...e)
-      return s
-    }, [])
-    .join('\n')
-  res.send(msg)
+  res.send(process.env.HOSTNAME)
 })
 app.listen(8080)
